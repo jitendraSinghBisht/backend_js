296ecd827b639670f1db029a620c2a54ccd32f36
Jitendra Singh Bisht <jitendrasinghbisht1404m@gmail.com>
2024-04-17 15:46:26 +0530

    update index route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,3 +1,21 @@
-import userRouter from './user.routes.js'
+import userRouter from "./user.routes.js";
+import healthcheckRouter from "./healthcheck.routes.js";
+import tweetRouter from "./tweet.routes.js";
+import subscriptionRouter from "./subscription.routes.js";
+import videoRouter from "./video.routes.js";
+import commentRouter from "./comment.routes.js";
+import likeRouter from "./like.routes.js";
+import playlistRouter from "./playlist.routes.js";
+import dashboardRouter from "./dashboard.routes.js";
 
-export {userRouter}
+export {
+  userRouter,
+  healthcheckRouter,
+  tweetRouter,
+  subscriptionRouter,
+  videoRouter,
+  commentRouter,
+  likeRouter,
+  playlistRouter,
+  dashboardRouter,
+};
