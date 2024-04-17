b73ffd27dc949247e4a998d8e36292916bf34576
Jitendra Singh Bisht <jitendrasinghbisht1404m@gmail.com>
2024-04-17 15:50:24 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -17,9 +17,27 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
-import { userRouter } from "./routes/index.routes.js";
+import {
+  userRouter,
+  healthcheckRouter,
+  tweetRouter,
+  subscriptionRouter,
+  videoRouter,
+  commentRouter,
+  likeRouter,
+  playlistRouter,
+  dashboardRouter,
+} from "./routes/index.routes.js";
 
 //routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export { app };
